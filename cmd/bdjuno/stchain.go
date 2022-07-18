2f63adab42ca76cbcffeb1b91e34766db9eec0c3
hong-pang <hongpang@qsnetwork.org>
2022-07-18 12:06:54 -0400

    added custom addresses parser

---
+++ cmd/bdjuno/stchain.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	junomessages "github.com/forbole/juno/v3/modules/messages"
+	pottypes "github.com/stratosnet/stratos-chain/x/pot/types"
+	//profilestypes "github.com/desmos-labs/desmos/v2/x/profiles/types"
+	registertypes "github.com/stratosnet/stratos-chain/x/register/types"
+	sdstypes "github.com/stratosnet/stratos-chain/x/sds/types"
+)
+
+// stchainMessageAddressesParser represents a parser able to get the addresses of the involved
+// account from a stchain message
+var stchainMessageAddressesParser = junomessages.JoinMessageParsers(
+	registerMessageAddressesParser,
+	potMessageAddressesParser,
+	sdsMessageAddressesParser,
+)
+
+// registerMessageAddressesParser represents a MessageAddressesParser for the x/register module
+func registerMessageAddressesParser(_ codec.Codec, cosmosMsg sdk.Msg) ([]string, error) {
+	switch msg := cosmosMsg.(type) {
+
+	case *registertypes.MsgCreateResourceNode:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgCreateMetaNode:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgRemoveResourceNode:
+		return []string{msg.ResourceNodeAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgRemoveMetaNode:
+		return []string{msg.MetaNodeAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgUpdateResourceNode:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgUpdateMetaNode:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgUpdateResourceNodeStake:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgUpdateMetaNodeStake:
+		return []string{msg.NetworkAddress, msg.OwnerAddress}, nil
+
+	case *registertypes.MsgMetaNodeRegistrationVote:
+		return []string{msg.CandidateNetworkAddress, msg.CandidateOwnerAddress,
+			msg.VoterNetworkAddress, msg.VoterOwnerAddress}, nil
+	}
+
+	return nil, junomessages.MessageNotSupported(cosmosMsg)
+}
+
+// profilesMessageAddressesParser represents a MessageAddressesParser for the x/pot module
+func potMessageAddressesParser(_ codec.Codec, cosmosMsg sdk.Msg) ([]string, error) {
+	switch msg := cosmosMsg.(type) {
+
+	case *pottypes.MsgVolumeReport:
+		return []string{msg.Reporter, msg.ReporterOwner}, nil
+
+	case *pottypes.MsgWithdraw:
+		return []string{msg.WalletAddress, msg.TargetAddress}, nil
+
+	case *pottypes.MsgFoundationDeposit:
+		return []string{msg.From}, nil
+
+	case *pottypes.MsgSlashingResourceNode:
+		return []string{msg.NetworkAddress, msg.WalletAddress}, nil
+	}
+
+	return nil, junomessages.MessageNotSupported(cosmosMsg)
+}
+
+// sdsMessageAddressesParser represents a MessageAddressesParser for the x/sds module
+func sdsMessageAddressesParser(_ codec.Codec, cosmosMsg sdk.Msg) ([]string, error) {
+	switch msg := cosmosMsg.(type) {
+
+	case *sdstypes.MsgFileUpload:
+		return []string{msg.From, msg.Reporter, msg.Uploader}, nil
+
+	case *sdstypes.MsgPrepay:
+		return []string{msg.Sender}, nil
+	}
+
+	return nil, junomessages.MessageNotSupported(cosmosMsg)
+}
--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/cosmos/iavl v0.17.3 // indirect
+	github.com/cosmos/ibc-go v1.2.3 // indirect
 	github.com/cosmos/ibc-go/v3 v3.0.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.11.1 // indirect
 	github.com/cosmos/ledger-go v0.9.2 // indirect
@@ -56,6 +57,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gballet/go-libpcsclite v0.0.0-20190607065134-2772fd86a8ff // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
@@ -98,6 +100,7 @@ require (
 	github.com/mimoo/StrobeGo v0.0.0-20181016162300-f8f6d4d2b643 // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/onsi/gomega v1.18.1 // indirect
